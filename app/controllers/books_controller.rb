be179bccfad6256818c9888d6502e68f76a9f963
YuukiTanaka <145189789+yuukitanaka006607@users.noreply.github.com>
2023-11-15 01:37:08 +0000

    投稿フォーム

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,24 @@
 class BooksController < ApplicationController
   def new
-    @book = Books.new
+  　@book = Book.new
   end
-  
-  
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/homes/about'
+  end
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
